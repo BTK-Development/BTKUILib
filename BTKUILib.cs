fa7ef4eb7de214468c343a0b3fce4d210019b1bc
ddakebono <gandvando@hotmail.com>
2024-09-08 01:13:54 -0500

    Bump to preview version

--- BTKUILib.cs
+++ BTKUILib.cs
@@ -13,7 +13,7 @@ namespace BTKUILib
         public const string Name = "BTKUILib";
         public const string Author = "BTK Development Team";
         public const string Company = "BTK Development";
-        public const string Version = "2.2.1";
+        public const string Version = "2.3.0-preview1";
     }
     
     internal class BTKUILib : MelonMod
